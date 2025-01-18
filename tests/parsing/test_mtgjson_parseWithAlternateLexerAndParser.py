82b36899e6126220a561f9836a8011b5e7b35f0c
Reed Milewicz <rmmilewi@gmail.com>
2025-01-18 10:16:33 -0700

    Fixed instances of words like 'and' and 'to' not being parsed properly when the rules don't use the lexer-assigned terms (writing 'to'i vs. PREPOSITION_TO in grammar rules); mana symbols now count as value terms, allowing them to be used in mathematical operations/comparisons (like 'this spell costs *{1} less* to cast'); 'that' can now be used prepositionally within declarations (like 'prevent all damage *that* it would deal'); expanded prepositionalattacheddeclaration to include expressions (like 'prevent all damage that *would be dealt*'); kick added as verb (and removed from keyword status modifiers); 'instead' can now be used in place of 'then' in a conditional sequence (like 'if this spell was kicked, *instead* draw a card'); 'a number' is now a valueterm (this indicates that valueterm needs to be overhauled, maybe even have value expressions); value comparisons and operations can now take declarations (e.g., 'equal to *its power*'); added 'to name' as verb

--- tests/parsing/test_mtgjson_parseWithAlternateLexerAndParser.py
+++ tests/parsing/test_mtgjson_parseWithAlternateLexerAndParser.py
@@ -48,67 +48,14 @@ class TestGrammarAndParser(unittest.TestCase):
                      })
         parser = compilerUsingIntegratedGrammar.getParser()
         preprocessor = compilerUsingIntegratedGrammar.getPreprocessor()
-        cardsToTest = [ #TODO: Each card now needs to have a card name tuple (name,text), so these won't work as written.
-            "Clues you control are equipment in addition to their other types.",  # Armed with Proof
-            "If two cards that share all their card types were milled this way, sacrifice ~.", #Demonic Covenant
-            "During your next turn, you may draw a card.",
-            "For each artifact and/or enchantment you control, that creature gets +1/+1.",
-            "For each artifact you control, draw a card.",
-            "For each artifact and/or enchantment you control, enchanted creature gets +1/+1.",
-            "Enchanted creature gets +1/+1 for each artifact and/or enchantment you control.", #All That Glitters
-            "As ~ enters, it becomes your choice of a 3/3 artifact creature, a 2/2 artifact creature with flying, or a 1/6 Wall artifact creature with defender.",
-            "As ~ enters, it becomes your choice of a 3/3 artifact creature, a 2/2 artifact creature with flying, or a 1/6 Wall artifact creature with defender in addition to its other types.", #Primal Clay
-            "Exile target nonland permanent you control.",
-            "Exile target nonland permanent an opponent controls until ~ leaves the battlefield.",
-            "when ~ enters, exile target nonland permanent an opponent controls until ~ leaves the battlefield.\nyour opponents can not cast spells with the same name as the exiled card.",  # Ixalan's binding
-            "booksmarts — draw a card. if you control a wizard, draw an additional card. (aren't you clever!)", #This has an ambiguity.
-            "flash\nward {2}\nprobing telepathy — whenever a creature entering under an opponent's control causes a triggered ability of that creature to trigger, you may copy that ability. You may choose new targets for the copy.",# Aboleth Spawn
-            "−1: Target player draws a card.\n+10: Target player draws a card.\n−X: Target player draws a card.\n0: Target player draws a card.",
-            "They can not be attacked.",  # There's an ambiguity here that I need to figure out.
-            "Each creature has trample. Creatures can not gain flying. Creatures become enchantments. You may draw a card.",
-            "If you would lose the game, you win the game instead.",
-            "Roll a d6, a d8, and a d12.",  # coins and dice
-            "Roll a d6 and flip a coin.",  # coins and dice
-            "Choose an elf, goblin, and merfolk.",
-            "Roll a d6 and a d8.",
-            "Create a 1/1 red Mercenary creature token with \"{T}: Target creature you control gets +1/+0 until end of turn. Activate only as a sorcery.\".", #Quoted ability test.
-            "Sacrifice all blue creatures and black creatures, then draw a card.",
-            "Planeswalkers' loyalty abilities you activate cost an additional [+1] to activate.", #Carth the Lion
-            "{t}: put a +1/+1 counter on each artifact creature you control.", #Steel Overseer
-            "unless its controller pays {2}, counter target spell.",  # Quench (inverted)
-            "counter target spell unless its controller pays {2}.",  # Quench
-            "{t}: draw a card, then discard a card.",  # Merfolk Looter
-            "haste (haste is an ability)",
-            "Create a 1/1 green elf creature token under your control. Gain control of all elves.",# control as noun test
-            "destroy each bird on the battlefield.",
-            "draw a card for each bird on the battlefield.", #Airborne Aid
-            "reveal the player you chose.",
-            "when ~ enters, secretly choose an opponent.", #Stalking Leonin part 1
-            "reveal the player you chose: exile target creature that is attacking you if it is controlled by the chosen player. activate only once.", #Stalking Leonin part 2
-            "destroy target nonattacking creature.",
-            "when this creature dies, create a 1/1 black and green insect creature token with flying.", #Infestation Sage
-            "{T}: add {C}.\n{G/W}, {T}: add {G}{G}, {G}{W}, or {W}{W}.", #Wooded Bastion
-            "~ deals 4 damage to target creature and each other creature with the same name as that creature.", #Homing Lightning
-            "discard your hand.", #One With Nothing
-            "at the beginning of your upkeep, each opponent chooses money, friends, or secrets. for each player who chose money, you and that player each create a treasure token. for each player who chose friends, you and that player each create a 1/1 green and white citizen creature token. for each player who chose secrets, you and that player each draw a card.", #Master of Ceremonies
-            "~ can not be the target of white spells or abilities from white sources.\n{3}: put target nontoken rebel on the bottom of its owner's library.", #Rebel Informer
-            "return target creature card from your graveyard to the battlefield. if {G} was spent to cast this spell, that creature enters with an additional +1/+1 counter on it.", #Vigor Mortis
-            "{T}: add {G}.\n{T}: target 1/1 creature gets +1/+2 until end of turn.", #Pendlehaven
-            "reach\nwhenever ~ or another cat you control enters, you may destroy target artifact or enchantment.", #Qasali Slingers
-            "target nonattacking creature gains reach and deathtouch until end of turn. untap it.", #Ruthless Instincts
-            "destroy target nonblack creature.",  # Doom Blade
-            "draw two cards.",  # Divination
-            "{t}, sacrifice ~: add three mana of any one color.",  # Black Lotus
-            "+2: each player draws a card.\n+1: target player draws a card.\n+10: target player puts the top twenty cards of their library into their graveyard.", # Jace Beleren
-            "for each land you control, you gain 1 life.", #Bountiful Harvest
-            "{1}{u}{r}: return ~ to its owner's hand.\ncascade (when you cast this spell, exile cards from the top of your library until you exile a nonland card that costs less. You may cast it without paying its mana cost. Put the exiled cards on the bottom in a random order.)", #Etherium-Horn Sorcerer
-            "when ~ enters, mill three cards.\nwhen ~ dies, you may exile it. when you do, return target creature card from your graveyard to your hand.",
-            "flying, vigilance, lifelink", #Aerial Responder
-            "enchant creature\nenchanted creature is a treefolk with base power and toughness 0/4 and loses all abilities.", #Lignify,
-            "when ~ enters, tap target creature.",
-            "if a player would draw a card except the first one they draw in each of their draw steps, that player discards a card instead.", #Chains of Mephistopheles
-            "at the beginning of your end step, choose one — • you gain 1 life.\n• return target creature card with mana value 1 from your graveyard to the battlefield.", #Abiding Grace
-            "create a 2/2 white cat soldier creature token named OBJECTNAME with \“whenever you gain life, put a +1/+1 counter on OBJECTNAME.\”" #Ajani, Strength of the Pride
+        cardsToTest = [
+            ("Joust Through Snippet", "~ deals 3 damage to target attacking or blocking creature."),
+            ("Hare Apparent Snippet", "Create a number of 1/1 white Rabbit creature tokens equal to the number of other creatures you control named ~."),
+            ("Divine Resilience Snippet","If this spell was kicked, instead any number of target creatures you control gain indestructible until end of turn."),
+            ("Crystal Barricade Snippet","Prevent all noncombat damage that would be dealt to other creatures you control."),
+            ("Celestial Armor Snippet", "That creature gains hexproof and indestructible until end of turn."),
+            ("Claws Out Snippet","This spell costs {1} less to cast."),
+            ("Felidar Savior Snippet","Put a +1/+1 counter on each of up to two other target creatures you control.")
         ]
         if testAgainstFoundations:
             with open("tests/parsing/FDN.json", encoding='utf-8') as f:
